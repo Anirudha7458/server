8edad08bd7255e473dcfd1af46d0500159d9d8a7
Anirudha Patil <aniruddhap66@gmail.com>
2023-11-13 09:38:02 +0530

    first commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,18 +6,21 @@ const User = require("../models/user");
 router.post("/register", async (req, res) => {
   try {
     const newUser = new User(req.body);
-    const user = await newUser.save();
-
-    res.send("User Registered Successfully");
+    await newUser.save();
+    res.status(201).json({ message: "User Registered Successfully" });
   } catch (error) {
-    return res.status(400).json({ message: error });
+    if (error.code === 11000 && error.keyPattern && error.keyPattern.email) {
+      // Duplicate email error
+      return res.status(400).json({ message: "Email already exists. Please use a different email address." });
+    }
+    console.error(error);
+    return res.status(500).json({ message: "Registration failed", error: error.message });
   }
 });
-
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email: email, password: password });
+    const user = await User.findOne({ email, password });
     if (user) {
       const temp = {
         name: user.name,
@@ -25,22 +28,23 @@ router.post("/login", async (req, res) => {
         isAdmin: user.isAdmin,
         _id: user._id,
       };
-      res.send(temp);
+      res.status(200).json(temp); // 200 OK status for successful login
     } else {
-      return res.status(400).json({ message: "Login Failed" });
+      return res.status(401).json({ message: "Login Failed" }); // 401 Unauthorized status for failed login
     }
   } catch (error) {
-    return res.status(400).json({ message: error });
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error", error: error.message }); // 500 Internal Server Error status for server-side errors
   }
 });
 
-router.post("/getallusers", async (req, res) => {
+router.get("/getallusers", async (req, res) => {
   try {
     const users = await User.find();
-    res.send(users);
+    res.status(200).json(users); // 200 OK status for successful retrieval of users
   } catch (error) {
-    console.log(error);
-    return res.status(400).json({ message: error });
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error", error: error.message });
   }
 });
 
